9893b8bfd24a3102ca63ca83ddabb3a6b6f033d4
zxmon <zxmon3@gmail.com>
2021-08-30 15:33:19 +0900

    2.4.4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 function App() {
 
-  const name = '리액트';
-  return (
-    <>
+  const name = '뤼왝트';
+  return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
 
-      {name === '리액트' ? (
-
-        <h1>리액트입니다</h1>
-      ) : (
-        <h2>리액트가 아닙니다.</h2>
-
-      )}
-    </>
-  );
 }
 
 export default App;
