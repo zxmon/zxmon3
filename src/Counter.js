aacea62e8874217efa82e4ee3c970dbb4b25f35c
zxmon <zxmon3@gmail.com>
2021-08-30 17:32:03 +0900

    3.4.1.4

--- src/Counter.js
+++ src/Counter.js
@@ -16,15 +16,16 @@ class Counter extends Component {
                 <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button
                     onClick={() => {
-                        this.setState(prevState => {
-                            return {
-                                number: prevState.number + 1
-                            };
-                        });
-                        this.setState(prevState => ({
-                            number: prevState.number + 1
+                        this.setState(
+                            {
+                                number: number + 1
+                            },
 
-                        }));
+                            () => {
+                                console.log('방금 setState가 호출되었습니다.');
+                                console.log(this.state);
+                            }
+                        );
                     }}
                 >
                     +1
