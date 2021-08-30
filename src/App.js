46ed4d6d5a48d2e66fa657e0cc76283d2d741b7b
zxmon <zxmon3@gmail.com>
2021-08-30 17:17:45 +0900

    3.4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import MyComponent from "./MyComponent";
+import Counter from './Counter';
 
 const App = () => {
 
-  return (
-    <MyComponent name="React" favoriteNumber={1}>
-      리액트
-    </MyComponent>
-  );
+  return <Counter />;
+
 };
 
 export default App;
