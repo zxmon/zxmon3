b32c32fc69f99ab0d599e5ae50629f5e28fb8b90
zxmon <zxmon3@gmail.com>
2021-08-30 17:21:47 +0900

    3.4.1.1

--- src/Counter.js
+++ src/Counter.js
@@ -5,14 +5,16 @@ class Counter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            number: 0
+            number: 0,
+            fixedNumber: 0
         };
     }
     render() {
-        const { number } = this.state;
+        const { number, fixedNumber } = this.state;
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button
                     onClick={() => {
                         this.setState({ number: number + 1 });
@@ -22,7 +24,7 @@ class Counter extends Component {
                     +1
                 </button>
 
-            </div>
+            </div >
         );
 
     }
