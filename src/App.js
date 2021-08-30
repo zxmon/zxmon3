8a4519674f14e2cc8ef185eb53c3b97324ecd9b5
zxmon <zxmon3@gmail.com>
2021-08-30 15:34:51 +0900

    2.4.5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import './App.css';
+
 function App() {
 
-  const name = '뤼왝트';
-  return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
+  const name = undefined;
+  return <div>{name || '리액트'}</div>;
 
 }
 
