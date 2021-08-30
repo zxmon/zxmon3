9d4a0ddb9b33d220bfe60c0e24f3465477d88460
zxmon <zxmon3@gmail.com>
2021-08-30 16:38:13 +0900

    3.3.5

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,9 +1,10 @@
-const MyComponent = props => {
+const MyComponent = ({ name, children }) => {
+
 
     return (
         <div>
-            안녕하세요, 제 이름은 {props.name}입니다.<br />
-            children 값은 {props.children}
+            안녕하세요, 제 이름은 {name}입니다.<br />
+            children 값은 {children}
             입니다.
         </div>
     );
