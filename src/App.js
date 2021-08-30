c123ca4dd843cabd3e71c5869129ca84070e7086
zxmon <zxmon3@gmail.com>
2021-08-30 15:41:55 +0900

    2.4.7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
+import './App.css';
+
 function App() {
 
   const name = '리액트';
 
-  return (
-    <div
-      style={{
-        backgroundColor: 'black',
-
-        color: 'aqua',
-
-        fontSize: '48px',
-
-        fontWeight: 'bold',
-
-        padding: 16
-      }}
-    >
-
-      {name}
-    </div>
-  );
+  return <div className="react">{name}</div>;
 }
 
 export default App;
