2d1299ed4cc8b9ce12d2b5fbfa5430d55f497be0
zxmon <zxmon3@gmail.com>
2021-08-30 16:33:41 +0900

    3.3.3

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,11 @@
-const MyComponent = () => {
+const MyComponent = props => {
 
-    return <div>나의 새롭고 멋진 컴포넌트</div>;
+    return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
 
+};
+
+MyComponent.defaultProps = {
+    name: '기본 이름'
 };
 
 export default MyComponent;
