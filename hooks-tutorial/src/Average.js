75cd095d7971e39e881215085aefe7862fdb87fa
zxmon <zxmon3@gmail.com>
2021-09-05 08:28:21 +0900

    8.7

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback } from 'react';
+import { useState, useMemo, useCallback, useRef } from 'react';
  
 const getAverage = numbers => {
   console.log('평균값 계산 중..');
@@ -10,23 +10,26 @@ const getAverage = numbers => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
+  const inputEl = useRef(null);
  
   const onChange = useCallback(e => {
     setNumber(e.target.value);
-}, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
+  }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
 const onInsert = useCallback(() => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber('');
+    inputEl.current.focus();
   }, [number, list]); // number 혹은 list가 바뀌었을 때만 함수 생성
  
   const avg = useMemo(() => getAverage(list), [list]);
+ 
   return (
     <div>
-      <input value={number} onChange={onChange}  />
+      <input value={number} onChange={onChange} ref={inputEl} />
       <button onClick={onInsert}>등록</button>
       <ul>
-        {list.map((value, index) => (
+      {list.map((value, index) => (
           <li key={index}>{value}</li>
         ))}
       </ul>
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,37 +1,28 @@
-import React, { useReducer } from 'react';
- 
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value
-  };
-}
+import useInputs from './useInputs';
  
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: '',
     nickname: ''
-  });
-  const { name, nickname } = state;
-  const onChange = e => {
-    dispatch(e.target);
-  };
-  return (
+});
+const { name, nickname } = state;
+
+return (
+  <div>
+    <div>
+      <input name="name" value={name} onChange={onChange} />
+      <input name="nickname" value={nickname} onChange={onChange} />
+    </div>
     <div>
       <div>
-        <input name="name" value={name} onChange={onChange} />
-        <input name="nickname" value={nickname} onChange={onChange} />
+        <b>이름:</b> {name}
       </div>
       <div>
-        <div>
-          <b>이름:</b> {name}
-        </div>
-        <div>
-          <b>닉네임: </b>
-          {nickname}
-        </div>
+        <b>닉네임: </b>
+        {nickname}
       </div>
     </div>
+  </div>
   );
 };
  
