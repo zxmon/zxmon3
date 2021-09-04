832b383a5cdb7341085839cd0d1808167fa6d39f
zxmon <zxmon3@gmail.com>
2021-09-05 06:54:52 +0900

    6.4.3

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -9,27 +9,32 @@ const IterationSample = () => {
   ]);
   const [inputText, setInputText] = useState('');
   const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
-  
+ 
   const onChange = e => setInputText(e.target.value);
   const onClick = () => {
     const nextNames = names.concat({
-      id: nextId, // nextId 값을 id로 설정하고
-      text: inputText
-    });
-    setNextId(nextId + 1); // nextId 값에 1을 더해 준다.
-    setNames(nextNames); // names 값을 업데이트한다.
-    setInputText(''); // inputText를 비운다.
-  };
-
-  const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
-
-
-  return (
+        id: nextId, // nextId 값을 id로 설정하고
+        text: inputText
+      });
+      setNextId(nextId + 1); // nextId 값에 1을 더해 준다.
+      setNames(nextNames); // names 값을 업데이트한다.
+      setInputText(''); // inputText를 비운다.
+    };
+    const onRemove = id => {
+      const nextNames = names.filter(name => name.id !== id);
+      setNames(nextNames);
+    };
+    const namesList = names.map(name => (
+      <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+        {name.text}
+      </li>
+    ));
+    return (
       <>
-<input value={inputText} onChange={onChange} />
-<button onClick={onClick}>추가</button>
-  <ul>{namesList}</ul>;
-  </>
+        <input value={inputText} onChange={onChange} />
+        <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>
+    </>
   );
 };
  
