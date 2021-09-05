36b79ddcea6a18b133d126fa04eb0180abfbccf6
zxmon <zxmon3@gmail.com>
2021-09-05 09:42:40 +0900

    9.3.1

--- hooks-tutorial/styling-react/src/App.js
+++ hooks-tutorial/styling-react/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
  
 class App extends Component {
   render() {
     return (
       <div>
-                <SassComponent />
+        <CSSModule />
       </div>
     );
   }
 }
- 
+
 export default App;
