3bcb1ee27a718e871b91ef6f212476185c613d62
zxmon <zxmon3@gmail.com>
2021-09-05 09:47:52 +0900

    9.4

--- hooks-tutorial/styling-react/src/App.js
+++ hooks-tutorial/styling-react/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
  
 class App extends Component {
   render() {
     return (
       <div>
-        <CSSModule />
+        <StyledComponent />
       </div>
     );
   }
 }
-
+ 
 export default App;
