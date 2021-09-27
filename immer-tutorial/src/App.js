939fc281fc6034154072ca0b647a536fb6a8b079
zxmon <zxmon3@gmail.com>
2021-09-27 19:06:53 +0900

    12.1.4

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,4 +1,6 @@
 import { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
+ 
 const App = () => {
   const nextId = useRef(1);
   const [form, setForm] = useState({ name: '', username: '' });
@@ -11,13 +13,15 @@ const App = () => {
   const onChange = useCallback(
     e => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: [value]
-      });
+      setForm(
+        produce(form, draft => {
+          draft[name] = value;
+        })
+      );
     },
     [form]
   );
+ 
   // form 등록을 위한 함수
   const onSubmit = useCallback(
     e => {
@@ -29,10 +33,11 @@ const App = () => {
       };
  
       // array에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info)
-      });
+      setData(
+        produce(data, draft => {
+          draft.array.push(info);
+        })
+      );
  
       // form 초기화
       setForm({
@@ -47,14 +52,14 @@ const App = () => {
   // 항목을 삭제하는 함수
   const onRemove = useCallback(
     id => {
-      setData({
-        ...data,
-        array: data.array.filter(info => info.id !== id)
-      });
+      setData(
+        produce(data, draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        })
+      );
     },
     [data]
   );
- 
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -75,14 +80,14 @@ const App = () => {
       <div>
         <ul>
           {data.array.map(info => (
-                        <li key={info.id} onClick={() => onRemove(info.id)}>
-                        {info.username} ({info.name})
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            );
-          };
-           
-          export default App;
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+            {info.username} ({info.name})
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
+};
+
+export default App;
