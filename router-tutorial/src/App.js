3de85eeeb848827ea2fb5c7e3b29de1eb1633929
zxmon <zxmon3@gmail.com>
2021-09-27 19:36:01 +0900

    13.2.4

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import { Route } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+
+const App = () => {
+  return (
+    <div>
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/about" component={About} />
+    </div>
+  );
+};
+
+export default App;
