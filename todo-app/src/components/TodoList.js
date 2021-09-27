1a26d88486d2982c4247948d30b183e46a776925
zxmon <zxmon3@gmail.com>
2021-09-27 15:39:24 +0900

    10.3.1

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,14 +1,14 @@
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
-    return (
-      <div className="TodoList">
-        <TodoListItem />
-        <TodoListItem />
-        <TodoListItem />
-      </div>
-    );
-  };
-   
-  export default TodoList;
+const TodoList = ({ todos }) => {
+  return (
+    <div className="TodoList">
+      {todos.map(todo => (
+        <TodoListItem todo={todo} key={todo.id} />
+      ))}
+    </div>
+  );
+};
+ 
+export default TodoList;
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,19 +1,22 @@
 import {
-    MdCheckBoxOutlineBlank,
-    MdCheckBox,
-    MdRemoveCircleOutline,
-  } from 'react-icons/md';
-  import './TodoListItem.scss';
-   
-  const TodoListItem = () => {
-    return (
-      <div className="TodoListItem">
-        <div className="checkbox">
-          <MdCheckBoxOutlineBlank />
-          <div className="text">할 일</div>
-        </div>
-        <div className="remove">
-        <MdRemoveCircleOutline />
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames';
+import './TodoListItem.scss';
+ 
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
+ 
+  return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove">
+      <MdRemoveCircleOutline />
       </div>
     </div>
   );
