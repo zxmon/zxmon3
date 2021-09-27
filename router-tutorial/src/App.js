fc69a8d1da45e382346eec55134378bd6fcbcc60
zxmon <zxmon3@gmail.com>
2021-09-27 19:57:52 +0900

    13.4.1

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,15 +1,31 @@
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 const App = () => {
   return (
     <div>
-
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+          </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact={true} />
-      <Route path={['/about', '/info/']} component={About} />
+      <Route path={['/about', '/info']} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };
-
+ 
 export default App;
