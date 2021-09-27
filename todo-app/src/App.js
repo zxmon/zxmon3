f80209ba411950521f9c9dba973b9e20626e6776
zxmon <zxmon3@gmail.com>
2021-09-27 15:30:50 +0900

    10.2.3

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,12 @@
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
  
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
