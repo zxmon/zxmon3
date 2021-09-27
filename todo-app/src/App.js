39354b6aea91a633580b01fdb46e108eca2c08e0
zxmon <zxmon3@gmail.com>
2021-09-27 15:03:05 +0900

    10.2.2

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,12 @@
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
  
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
 };
  
 export default App;
