6158e885f6a049ebe2ec798bb1f95cea70f3db8b
zxmon <zxmon3@gmail.com>
2021-09-29 18:05:02 +0900

    13.6.2

--- router-tutorial/src/Profile.js
+++ router-tutorial/src/Profile.js
@@ -1,3 +1,6 @@
+import { withRouter } from 'react-router-dom';
+import WithRouterSample from './WithRouterSample';
+
 const data = {
     velopert: {
         name: '김민준',
@@ -22,6 +25,7 @@ const Profile = ({ match }) => {
           {username}({profile.name})
         </h3>
         <p>{profile.description}</p>
+        <WithRouterSample />
       </div>
     );
   };
--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,5 +1,6 @@
 import { Link, Route } from 'react-router-dom';
 import Profile from './Profile';
+import WithRouterSample from './WithRouterSample';
 
 const Profiles = () => {
     return (
@@ -20,6 +21,7 @@ const Profiles = () => {
           render={() => <div>사용자를 선택해 주세요.</div>}
         />
         <Route path="/profiles/:username" component={Profile} />
+        <WithRouterSample />
         </div>
   );
 };
