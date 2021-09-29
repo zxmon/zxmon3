ff64c8fa0e4f4f8aca2512df9d8f42ed10121c0e
zxmon <zxmon3@gmail.com>
2021-09-29 18:18:07 +0900

    13.6.4

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,19 +1,27 @@
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
-import WithRouterSample from './WithRouterSample';
+
 
 const Profiles = () => {
-    return (
-      <div>
-        <h3>사용자 목록:</h3>
-        <ul>
-          <li>
-            <Link to="/profiles/velopert">velopert</Link>
-          </li>
-          <li>
-            <Link to="/profiles/gildong">gildong</Link>
-          </li>
-        </ul>
+    const activeStyle = {
+        background: 'black',
+        color: 'white'
+      };
+      return (
+        <div>
+          <h3>사용자 목록:</h3>
+          <ul>
+            <li>
+              <NavLink activeStyle={activeStyle} to="/profiles/velopert">
+                velopert
+                </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={activeStyle} to="/profiles/gildong">
+            gildong
+          </NavLink>
+        </li>
+      </ul>
    
         <Route
           path="/profiles"
@@ -21,7 +29,6 @@ const Profiles = () => {
           render={() => <div>사용자를 선택해 주세요.</div>}
         />
         <Route path="/profiles/:username" component={Profile} />
-        <WithRouterSample />
         </div>
   );
 };
