7d078f980ee4d75dbf051f287663501c9de5c5cc
zxmon <zxmon3@gmail.com>
2021-10-02 06:07:19 +0900

    14.6.1

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,7 +1,19 @@
+import { useState, useCallback } from 'react';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
  
 const App = () => {
-  return <NewsList />;
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
+
+  return (
+    <>
+
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+
+    </>
+    );
 };
  
 export default App;
