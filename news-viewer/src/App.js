1c0403d5e74067dfeb2d8e587087bff76879b09d
zxmon <zxmon3@gmail.com>
2021-10-02 05:58:31 +0900

    14.4

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,26 +1,7 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
  
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=d7259441d7aa4d1fb5f4f59b29cc9ed0',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-    </div>
-  );
+  return <NewsList />;
 };
  
 export default App;
