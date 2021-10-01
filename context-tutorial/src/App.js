9866bccfbd7d30f1ddc6fc86cc6b83a532b21ba3
zxmon <zxmon3@gmail.com>
2021-10-02 06:59:04 +0900

    15.3.3

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,11 +1,14 @@
 import ColorBox from './components/ColorBox';
 import { ColorProvider } from './contexts/color';
+import SelectColors from './components/SelectColors';
+ 
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
-        </div>
+      </div>
     </ColorProvider>
   );
 };
