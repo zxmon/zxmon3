7bad5692403430242d311d4245248db4cb0b306e
zxmon <zxmon3@gmail.com>
2021-10-02 08:38:37 +0900

    18.1

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,11 @@
+import CounterContainer from './containers/CounterContainer';
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer />
+    </div>
+  );
+};
+
+export default App;
---
+++ learn-redux-middleware/src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
