c5c22623ccbf0ab56d5ff12101f628f179f6358f
zxmon <zxmon3@gmail.com>
2021-10-02 06:20:22 +0900

    14.7.2

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,19 +1,8 @@
-import { useState, useCallback } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
  
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
-
-  return (
-    <>
-
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-
-    </>
-    );
+  return <Route path="/:category?" component={NewsPage} />;
 };
  
 export default App;
