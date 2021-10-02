c0050a3c5d1e14755214d7c3122122cbedcb68ec
zxmon <zxmon3@gmail.com>
2021-10-02 14:09:57 +0900

    20.4.2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route } from 'react-router-dom';
 import Menu from './components/Menu';
 import RedPage from './pages/RedPage';
 import BluePage from './pages/BluePage';
+import UsersPage from './pages/UsersPage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <hr />
       <Route path="/red" component={RedPage} />
       <Route path="/blue" component={BluePage} />
+      <Route path="/users" component={UsersPage} />
     </div>
   );
 };
