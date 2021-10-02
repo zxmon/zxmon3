e8570d3b697a51ef8728da4be07a31a18c1a8fc0
zxmon <zxmon3@gmail.com>
2021-10-02 09:34:50 +0900

    18.3.2.4

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { delay, put, takeEvery, takeLatest, select } from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -14,9 +14,11 @@ export const increaseAsync = createAction(INCREASE_ASYNC, () => undefined);
 export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 
 function* increaseSaga() {
-  yield delay(1000); // 1초를 기다립니다.
-  yield put(increase()); // 특정 액션을 디스패치합니다.
-}
+    yield delay(1000); // 1초를 기다립니다.
+    yield put(increase()); // 특정 액션을 디스패치합니다.
+    const number = yield select(state => state.counter); // state는 스토어 상태를 의미함
+    console.log(`현재 값은 ${number}입니다.`);
+  }
 
 function* decreaseSaga() {
   yield delay(1000); // 1초를 기다립니다.
