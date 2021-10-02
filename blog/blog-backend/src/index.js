b59bac54a7ff1c7bb03bfd7c5159e7069bc81738
zxmon <zxmon3@gmail.com>
2021-10-02 17:08:54 +0900

    21.3.2.2

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,20 +2,19 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-app.use((ctx, next) => {
+app.use(async (ctx, next) => {
   console.log(ctx.url);
   console.log(1);
-  if (ctx.query.authorized !== '1') {
+  if (ctx.query.authorized != = '1') {
     ctx.status = 401; // Unauthorized
     return;
   }
-  next().then(() => {
-    console.log('END');
-  });
+  await next();
+  console.log('END');
 });
 
 app.use((ctx, next) => {
-    console.log(2);
+  console.log(2);
   next();
 });
 
