8b973d05c11bfc3498196744a14cb6edf988c6c6
zxmon <zxmon3@gmail.com>
2021-10-02 14:03:37 +0900

    20.3.5

--- src/index.server.js
+++ src/index.server.js
@@ -3,7 +3,44 @@ import express from 'express';
 import { StaticRouter } from 'react-router-dom';
 import App from './App';
 import path from 'path';
+import fs from 'fs';
 
+// asset-manifest.json에서 파일 경로들을 조회합니다.
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
+);
+
+const chunks = Object.keys(manifest.files)
+  .filter(key => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
+  .map(key => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
+  .join(''); // 합침
+
+function createPage(root) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1,shrink-to-fit=no"
+    />
+    <meta name="theme-color" content="#000000" />
+    <title>React App</title>
+    <link href="${manifest.files['main.css']}" rel="stylesheet" />
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root">
+      ${root}
+    </div>
+    <script src="${manifest.files['runtime-main.js']}"></script>
+    ${chunks}
+    <script src="${manifest.files['main.js']}"></script>
+  </body>
+  </html>
+    `;
+}
 const app = express();
 
 // 서버 사이드 렌더링을 처리할 핸들러 함수입니다.
@@ -14,10 +51,10 @@ const serverRender = (req, res, next) => {
   const jsx = (
     <StaticRouter location={req.url} context={context}>
       <App />
-    </StaticRouter>
+      </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-  res.send(root); // 결과물을 응답합니다.
+  res.send(createPage(root)); // 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve('./build'), {
