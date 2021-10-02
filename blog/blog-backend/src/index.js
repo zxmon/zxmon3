a842789bdc24efae234aab92ba7771f88d290177
zxmon <zxmon3@gmail.com>
2021-10-02 17:06:39 +0900

    21.3.2

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,6 +2,21 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+app.use((ctx, next) => {
+  console.log(ctx.url);
+  console.log(1);
+  if (ctx.query.authorized !== '1') {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  next();
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
 app.use(ctx => {
   ctx.body = 'hello world';
 });
