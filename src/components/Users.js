fea8bae941b9f0299f286c82edd1e8101c2e3d7e
zxmon <zxmon3@gmail.com>
2021-10-02 14:48:02 +0900

    20.4.4

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+
 const Users = ({ users }) => {
     if (!users) return null; // users가 유효하지 않다면 아무것도 보여 주지 않음
     return (
--- src/index.server.js
+++ src/index.server.js
@@ -4,6 +4,11 @@ import { StaticRouter } from 'react-router-dom';
 import App from './App';
 import path from 'path';
 import fs from 'fs';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './modules';
+import PreloadContext from './lib/PreloadContext';
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
@@ -44,16 +49,34 @@ function createPage(root) {
 const app = express();
 
 // 서버 사이드 렌더링을 처리할 핸들러 함수입니다.
-const serverRender = (req, res, next) => {
+const serverRender = async (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줍니다.
 
   const context = {};
+  const store = createStore(rootReducer, applyMiddleware(thunk));
+
+  const preloadContext = {
+    done: false,
+    promises: []
+  };
   const jsx = (
-    <StaticRouter location={req.url} context={context}>
-      <App />
-      </StaticRouter>
+    <PreloadContext.Provider value={preloadContext}>
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    </PreloadContext.Provider>
   );
-  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
+
+  ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한번 렌더링합니다.
+  try {
+    await Promise.all(preloadContext.promises); // 모든 프로미스를 기다립니다.
+  } catch (e) {
+    return res.status(500);
+  }
+  preloadContext.done = true;
+  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 합니다.
   res.send(createPage(root)); // 결과물을 응답합니다.
 };
 
