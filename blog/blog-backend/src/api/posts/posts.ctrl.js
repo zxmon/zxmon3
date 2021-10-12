62dde676e2a4bfe85d73175c0e50cacb9ddcf2e9
zxmon <zxmon3@gmail.com>
2021-10-12 16:14:29 +0900

    22.10.3

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -58,12 +58,16 @@ export const write = async ctx => {
 */
 export const list = async ctx => {
   try {
-    const posts = await Post.find().exec();
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .exec();
     ctx.body = posts;
   } catch (e) {
     ctx.throw(500, e);
   }
 };
+
 
 /*
   GET /api/posts/:id
