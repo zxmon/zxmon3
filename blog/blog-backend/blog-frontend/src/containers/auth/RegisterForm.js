5e2e6ac56d5fda36e3073677d041e55dbb3dd908
zxmon <zxmon3@gmail.com>
2021-10-12 20:15:58 +0900

    24.2.4

--- blog/blog-backend/blog-frontend/src/containers/auth/RegisterForm.js
+++ blog/blog-backend/blog-frontend/src/containers/auth/RegisterForm.js
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form, auth, authError } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
     auth: auth.auth,
-    authError: auth.authError
+    authError: auth.authError,
+    user: user.user
   }));
   // 인풋 변경 이벤트 핸들러
   const onChange = e => {
@@ -48,8 +51,16 @@ const RegisterForm = () => {
     if (auth) {
       console.log('회원가입 성공');
       console.log(auth);
+      dispatch(check());
     }
-  }, [auth, authError]);
+  }, [auth, authError, dispatch]);
+
+  // user 값이 잘 설정되었는지 확인
+  useEffect(() => {
+    if (user) {
+      history.push('/'); // 홈 화면으로 이동
+    }
+  }, [history, user]);
 
   return (
     <AuthForm
@@ -61,4 +72,4 @@ const RegisterForm = () => {
       );
     };
     
-    export default RegisterForm;
+export default withRouter(RegisterForm);
