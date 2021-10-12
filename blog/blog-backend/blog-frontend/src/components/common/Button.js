7dbb28dba6814d4a0f83714864f3158e8748b896
zxmon <zxmon3@gmail.com>
2021-10-12 20:30:54 +0900

    24.3.1

--- blog/blog-backend/blog-frontend/src/components/common/Button.js
+++ blog/blog-backend/blog-frontend/src/components/common/Button.js
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { withRouter } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 
 const StyledButton = styled.button`
@@ -10,31 +11,41 @@ const StyledButton = styled.button`
   color: white;
   outline: none;
   cursor: pointer;
-
   background: ${palette.gray[8]};
   &:hover {
     background: ${palette.gray[6]};
-}
+  }
 
-${props =>
-  props.fullWidth &&
-  css`
-    padding-top: 0.75rem;
-    padding-bottom: 0.75rem;
-    width: 100%;
-    font-size: 1.125rem;
-  `}
+  ${props =>
+    props.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
 
-${props =>
-  props.cyan &&
-  css`
-    background: ${palette.cyan[5]};
-    &:hover {
-      background: ${palette.cyan[4]};
-    }
-  `}
+  ${props =>
+    props.cyan &&
+    css`
+      background: ${palette.cyan[5]};
+      &:hover {
+        background: ${palette.cyan[4]};
+      }
+    `}
 `;
 
-const Button = props => <StyledButton {...props} />;
+const Button = ({ to, history, ...rest }) => {
+  const onClick = e => {
+    // to가 있다면 to로 페이지 이동
+    if (to) {
+      history.push(to);
+    }
+    if (rest.onClick) {
+      rest.onClick(e);
+    }
+  };
+  return <StyledButton {...rest} onClick={onClick} />;
+};
 
-export default Button;
+export default withRouter(Button);
