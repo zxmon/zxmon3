ff69e4129672dcf5d9d01ca459036c59b9bfddf6
zxmon <zxmon3@gmail.com>
2021-10-12 16:13:10 +0900

    22.10.1

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -6,17 +6,19 @@ import mongoose from 'mongoose';
 
 import api from './api';
 
+
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
-.connect(MONGO_URI, { useNewUrlParser: true})
+.connect(MONGO_URI, { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to MongoDB');
+    
   })
   .catch(e => {
     console.error(e);
-});
+  });
 
 const app = new Koa();
 const router = new Router();
