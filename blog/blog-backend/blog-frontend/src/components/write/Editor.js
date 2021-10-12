e3899a87f788f05a4d0bd989c85e208e7bd4c837
zxmon <zxmon3@gmail.com>
2021-10-12 21:08:27 +0900

    25.3.1

--- blog/blog-backend/blog-frontend/src/components/write/Editor.js
+++ blog/blog-backend/blog-frontend/src/components/write/Editor.js
@@ -32,35 +32,52 @@ const QuillWrapper = styled.div`
   }
 `;
 
-const Editor = () => {
-  const quillElement = useRef(null); // Quill을 적용할 DivElement를 설정
-  const quillInstance = useRef(null); // Quill 인스턴스를 설정
+const Editor = ({ title, body, onChangeField }) => {
+    const quillElement = useRef(null); // Quill을 적용할 DivElement를 설정
+    const quillInstance = useRef(null); // Quill 인스턴스를 설정
+  
+    useEffect(() => {
+      quillInstance.current = new Quill(quillElement.current, {
+        theme: 'bubble',
+        placeholder: '내용을 작성하세요...',
+        modules: {
+          // 더 많은 옵션
+          // https://quilljs.com/docs/modules/toolbar/ 참고
+          toolbar: [
+            [{ header: '1' }, { header: '2' }],
+            ['bold', 'italic', 'underline', 'strike'],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            ['blockquote', 'code-block', 'link', 'image'],
+          ],
+        },
+      });
+  
+      // quill에 text-change 이벤트 핸들러 등록
+      // 참고: https://quilljs.com/docs/api/#events
+      const quill = quillInstance.current;
+      quill.on('text-change', (delta, oldDelta, source) => {
+        if (source === 'user') {
+          onChangeField({ key: 'body', value: quill.root.innerHTML });
+        }
+      });
+    }, [onChangeField]);
+  
+    const onChangeTitle = e => {
+      onChangeField({ key: 'title', value: e.target.value });
+    };
+  
+    return (
+      <EditorBlock>
+        <TitleInput
+          placeholder="제목을 입력하세요"
+          onChange={onChangeTitle}
+          value={title}
+        />
+        <QuillWrapper>
+          <div ref={quillElement} />
+        </QuillWrapper>
+      </EditorBlock>
+    );
+  };
 
-  useEffect(() => {
-    quillInstance.current = new Quill(quillElement.current, {
-      theme: 'bubble',
-      placeholder: '내용을 작성하세요...',
-      modules: {
-        // 더 많은 옵션
-        // https://quilljs.com/docs/modules/toolbar/ 참고
-        toolbar: [
-          [{ header: '1' }, { header: '2' }],
-          ['bold', 'italic', 'underline', 'strike'],
-          [{ list: 'ordered' }, { list: 'bullet' }],
-          ['blockquote', 'code-block', 'link', 'image'],
-        ],
-      },
-    });
-  }, []);
-
-  return (
-    <EditorBlock>
-      <TitleInput placeholder="제목을 입력하세요" />
-      <QuillWrapper>
-        <div ref={quillElement} />
-      </QuillWrapper>
-    </EditorBlock>
-  );
-};
-
-export default Editor;
+  export default Editor;
--- blog/blog-backend/blog-frontend/src/pages/WritePage.js
+++ blog/blog-backend/blog-frontend/src/pages/WritePage.js
@@ -1,12 +1,12 @@
-import Editor from '../components/write/Editor';
 import TagBox from '../components/write/TagBox';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
+import EditorContainer from '../containers/write/EditorContainer';
 
 const WritePage = () => {
   return (
     <Responsive>
-      <Editor />
+      <EditorContainer />
       <TagBox />
       <WriteActionButtons />
     </Responsive>
