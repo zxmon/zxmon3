2eadd6e30e9cb8bf00e3da319be651b6b0efe489
zxmon <zxmon3@gmail.com>
2021-10-12 15:42:04 +0900

    22.3.2

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,8 +1,22 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 
 const api = require('./api');
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+.connect(MONGO_URI, { useNewUrlParser: true})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.error(e);
+});
 
 const app = new Koa();
 const router = new Router();
@@ -16,6 +30,8 @@ app.use(bodyParser());
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+// PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
