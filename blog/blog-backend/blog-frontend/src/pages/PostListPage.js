6046e50a235ed8482df9aa8486c70ba62a897b05
zxmon <zxmon3@gmail.com>
2021-10-12 22:00:51 +0900

    26.2.1

--- blog/blog-backend/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-backend/blog-frontend/src/pages/PostListPage.js
@@ -1,10 +1,11 @@
 import HeaderContainer from '../containers/common/HeaderContainer';
+import PostList from '../components/posts/PostList';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
-      <div>안녕하세요.</div>
+      <PostList />
     </>
   );
 };
