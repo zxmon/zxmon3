ccb1c0724d9a5c1a78c2b9c1f79e5012d7cb222c
zxmon <zxmon3@gmail.com>
2021-10-12 16:16:10 +0900

    22.10.6

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -57,7 +57,7 @@ export const write = async ctx => {
   GET /api/posts
 */
 export const list = async ctx => {
-  // query는 문자열이기 때문에 숫자로 변환해 주어야 합니다.
+// query는 문자열이기 때문에 숫자로 변환해 주어야 합니다.
   // 값이 주어지지 않았다면 1을 기본으로 사용합니다.
   const page = parseInt(ctx.query.page || '1', 10);
 
@@ -71,10 +71,15 @@ export const list = async ctx => {
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
+      .lean()
       .exec();
     const postCount = await Post.countDocuments().exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
-    ctx.body = posts;
+    ctx.body = posts.map(post => ({
+      ...post,
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+    }));
   } catch (e) {
     ctx.throw(500, e);
   }
