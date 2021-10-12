35babc124e07b70c5bc6d77e2e3b43e5342ebff5
zxmon <zxmon3@gmail.com>
2021-10-12 17:16:36 +0900

    23.5.1

--- blog/blog-backend/src/models/post.js
+++ blog/blog-backend/src/models/post.js
@@ -1,6 +1,4 @@
-import mongoose from 'mongoose';
-
-const { Schema } = mongoose;
+import mongoose, { Schema } from 'mongoose';
 
 const PostSchema = new Schema({
   title: String,
@@ -10,7 +8,10 @@ const PostSchema = new Schema({
     type: Date,
     default: Date.now, // 현재 날짜를 기본값으로 지정
   },
+  user: {
+    _id: mongoose.Types.ObjectId,
+    username: String,
+  },
 });
-
 const Post = mongoose.model('Post', PostSchema);
 export default Post;
