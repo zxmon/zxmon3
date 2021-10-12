cab29616544220247f016cfc7db2a2f7613a4b5f
zxmon <zxmon3@gmail.com>
2021-10-12 22:31:35 +0900

    26.2.2

--- blog/blog-backend/blog-frontend/src/components/posts/PostList.js
+++ blog/blog-backend/blog-frontend/src/components/posts/PostList.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import Responsive from '../common/Responsive';
 import Button from '../common/Button';
 import palette from '../../lib/styles/palette';
@@ -39,33 +40,48 @@ p {
 }
 `;
 
-const PostItem = () => {
+const PostItem = ({ post }) => {
+    const { publishedDate, user, tags, title, body, _id } = post;
     return (
       <PostItemBlock>
-        <h2>제목</h2>
-        <SubInfo username="username" publishedDate={new Date()} />
-        <Tags tags={['태그1', '태그2', '태그3']} />
-        <p>포스트 내용의 일부분..</p>
+        <h2>
+          <Link to={`/@${user.username}/${_id}`}>{title}</Link>
+        </h2>
+        <SubInfo
+          username={user.username}
+          publishedDate={new Date(publishedDate)}
+        />
+        <Tags tags={tags} />
+        <p>{body}</p>
       </PostItemBlock>
     );
   };
   
+  const PostList = ({ posts, loading, error, showWriteButton }) => {
+    // 에러 발생 시
+    if (error) {
+      return <PostListBlock>에러가 발생했습니다.</PostListBlock>;
+    }
   
-  const PostList = () => {
     return (
       <PostListBlock>
         <WritePostButtonWrapper>
-          <Button cyan to="/write">
-            새 글 작성하기
+          {showWriteButton && (
+            <Button cyan to="/write">
+                새 글 작성하기
           </Button>
-        </WritePostButtonWrapper>
+        )}
+      </WritePostButtonWrapper>
+      {/*  로딩 중이 아니고, 포스트 배열이 존재할 때만 보여 줌 */}
+      {!loading && posts && (
         <div>
-          <PostItem />
-          <PostItem />
-          <PostItem />
+          {posts.map(post => (
+            <PostItem post={post} key={post._id} />
+          ))}
         </div>
-      </PostListBlock>
-    );
-  };
-  
-  export default PostList;
+      )}
+    </PostListBlock>
+  );
+};
+
+export default PostList;
--- blog/blog-backend/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-backend/blog-frontend/src/pages/PostListPage.js
@@ -1,11 +1,11 @@
 import HeaderContainer from '../containers/common/HeaderContainer';
-import PostList from '../components/posts/PostList';
+import PostListContainer from '../containers/posts/PostListContainer';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
-      <PostList />
+      <PostListContainer />
     </>
   );
 };
