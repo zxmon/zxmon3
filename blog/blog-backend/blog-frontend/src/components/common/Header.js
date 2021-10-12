7b9320ae1e3c98dd46830e996b4941c313773dd4
zxmon <zxmon3@gmail.com>
2021-10-12 20:45:01 +0900

    24.3.3

--- blog/blog-backend/blog-frontend/src/components/common/Header.js
+++ blog/blog-backend/blog-frontend/src/components/common/Header.js
@@ -41,29 +41,29 @@ const UserInfo = styled.div`
   margin-right: 1rem;
 `;
 
-const Header = ({ user }) => {
-  return (
-    <>
-      <HeaderBlock>
-        <Wrapper>
-          <Link to="/" className="logo">
-            REACTERS
-          </Link>
-          {user ? (
-            <div className="right">
-              <UserInfo>{user.username}</UserInfo>
-              <Button>로그아웃</Button>
-            </div>
-          ) : (
-            <div className="right">
-              <Button to="/login">로그인</Button>
-            </div>
-          )}
-        </Wrapper>
-      </HeaderBlock>
-      <Spacer />
+const Header = ({ user, onLogout }) => {
+    return (
+      <>
+        <HeaderBlock>
+          <Wrapper>
+            <Link to="/" className="logo">
+              REACTERS
+            </Link>
+            {user ? (
+              <div className="right">
+                <UserInfo>{user.username}</UserInfo>
+                <Button onClick={onLogout}>로그아웃</Button>
+              </div>
+            ) : (
+              <div className="right">
+                <Button to="/login">로그인</Button>
+              </div>
+            )}
+          </Wrapper>
+        </HeaderBlock>
+        <Spacer />
       </>
-  );
+    );
 };
 
 export default Header;
