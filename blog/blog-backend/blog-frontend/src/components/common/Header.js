1c37adcb593d1b91a58620b58106d1bfd271e210
zxmon <zxmon3@gmail.com>
2021-10-12 20:36:08 +0900

    24.3.2.1

--- blog/blog-backend/blog-frontend/src/components/common/Header.js
+++ blog/blog-backend/blog-frontend/src/components/common/Header.js
@@ -36,22 +36,34 @@ const Spacer = styled.div`
 height: 4rem;
 `;
 
-const Header = () => {
-    return (
-      <>
-        <HeaderBlock>
-          <Wrapper>
-            <Link to="/" className="logo">
-              REACTERS
-            </Link>
+const UserInfo = styled.div`
+  font-weight: 800;
+  margin-right: 1rem;
+`;
+
+const Header = ({ user }) => {
+  return (
+    <>
+      <HeaderBlock>
+        <Wrapper>
+          <Link to="/" className="logo">
+            REACTERS
+          </Link>
+          {user ? (
+            <div className="right">
+              <UserInfo>{user.username}</UserInfo>
+              <Button>로그아웃</Button>
+            </div>
+          ) : (
             <div className="right">
               <Button to="/login">로그인</Button>
             </div>
-          </Wrapper>
-        </HeaderBlock>
-        <Spacer />
+          )}
+        </Wrapper>
+      </HeaderBlock>
+      <Spacer />
       </>
-    );
-  };
-  
-  export default Header;
+  );
+};
+
+export default Header;
