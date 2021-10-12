e41c26d8f91b9eee7585792c2ab5eda0bf281bf3
zxmon <zxmon3@gmail.com>
2021-10-12 21:01:32 +0900

    25.2.1

--- blog/blog-backend/blog-frontend/src/pages/WritePage.js
+++ blog/blog-backend/blog-frontend/src/pages/WritePage.js
@@ -1,10 +1,12 @@
 import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
 import Responsive from '../components/common/Responsive';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
+      <TagBox />
     </Responsive>
   );
 };
