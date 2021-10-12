e882226e858a70714360c8d14c96bac555a9161b
zxmon <zxmon3@gmail.com>
2021-10-12 16:10:57 +0900

    22.9.2

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,17 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request
+    return;
+  }
+  return next();
+};
 
 /*
   POST /api/posts
@@ -9,6 +22,23 @@ import Post from '../../models/post';
   }
 */
 export const write = async ctx => {
+  const schema = Joi.object().keys({
+    // 객체가 다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), // required()가 있으면 필수 항목
+    body: Joi.string().required(),
+    tags: Joi.array()
+      .items(Joi.string())
+      .required(), // 문자열로 이루어진 배열
+  });
+
+  // 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
@@ -22,7 +52,6 @@ export const write = async ctx => {
     ctx.throw(500, e);
   }
 };
-
 
 /*
   GET /api/posts
@@ -53,7 +82,55 @@ export const read = async ctx => {
   }
 };
 
+/*
+  DELETE /api/posts/:id
+*/
+export const remove = async ctx => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
-export const remove = ctx => {};
+/*
+  PATCH /api/posts/:id
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정', '태그']
+  }
+*/
+export const update = async ctx => {
+  const { id } = ctx.params;
+// write에서 사용한 schema와 비슷한데, required()가 없습니다.
+const schema = Joi.object().keys({
+  title: Joi.string(),
+  body: Joi.string(),
+  tags: Joi.array().items(Joi.string()),
+});
 
-export const update = ctx => {};
+// 검증하고 나서 검증 실패인 경우 에러 처리
+const result = schema.validate(ctx.request.body);
+if (result.error) {
+  ctx.status = 400; // Bad Request
+  ctx.body = result.error;
+  return;
+}
+
+try {
+  const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+    new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+    // false일 때는 업데이트되기 전의 데이터를 반환합니다.
+  }).exec();
+  if (!post) {
+    ctx.status = 404;
+    return;
+  }
+  ctx.body = post;
+} catch (e) {
+  ctx.throw(500, e);
+}
+};
