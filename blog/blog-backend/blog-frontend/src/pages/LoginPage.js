150d72f0e5e2f1b6db3c75b81e334b4f2563a90a
zxmon <zxmon3@gmail.com>
2021-10-12 19:19:54 +0900

    24.2.1

--- blog/blog-backend/blog-frontend/src/pages/LoginPage.js
+++ blog/blog-backend/blog-frontend/src/pages/LoginPage.js
@@ -2,11 +2,11 @@ import AuthTemplate from '../components/auth/AuthTemplate';
 import AuthForm from '../components/auth/AuthForm';
 
 const LoginPage = () => {
-  return (
-    <AuthTemplate>
-      <AuthForm />
-    </AuthTemplate>
-  );
-};
-
-export default LoginPage;
+    return (
+      <AuthTemplate>
+        <AuthForm type="login" />
+      </AuthTemplate>
+    );
+  };
+  
+  export default LoginPage;
--- blog/blog-backend/blog-frontend/src/pages/RegisterPage.js
+++ blog/blog-backend/blog-frontend/src/pages/RegisterPage.js
@@ -4,7 +4,7 @@ import AuthForm from '../components/auth/AuthForm';
 const RegisterPage = () => {
   return (
     <AuthTemplate>
-      <AuthForm />
+      <AuthForm type="register" />
     </AuthTemplate>
   );
 };
