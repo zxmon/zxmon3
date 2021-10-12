a9615a9755ff4485cecfca767f72618b383ed701
zxmon <zxmon3@gmail.com>
2021-10-12 21:56:00 +0900

    26.1.2

--- blog/blog-backend/blog-frontend/src/pages/PostPage.js
+++ blog/blog-backend/blog-frontend/src/pages/PostPage.js
@@ -1,11 +1,11 @@
 import HeaderContainer from '../containers/common/HeaderContainer';
-import PostViewer from '../components/post/PostViewer';
+import PostViewerContainer from '../containers/post/PostViewerContainer';
 
 const PostPage = () => {
   return (
     <>
-          <HeaderContainer />
-      <PostViewer />
+      <HeaderContainer />
+      <PostViewerContainer />
     </>
   );
 };
